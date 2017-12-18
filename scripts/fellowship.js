5cf8f6e1284bb055bc68192b3d698a4fb0b1645d
Michael Weber <webermn15@gmail.com>
2017-12-18 16:16:46 -0600

    Part 1 - makeMiddleEarth complete

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -21,3 +21,50 @@ var body = document.body;
 
 
 
+//created makemiddleearth function that, when called, fills in missing pieces of middle earth
+var makeMiddleEarth = function() {
+	//creates a new section & gives it an id of 'middle-earth'
+	let newSection = document.createElement('section');
+	newSection.id = "middle-earth";
+	//loops through the lands array, creating new article and h1 tags and appending them to the section
+	for (let i = 0; i < lands.length; i++) {
+		let tempArticle = document.createElement('article');
+		let newLand = document.createElement('h1');
+		newLand.innerHTML = lands[i];
+		tempArticle.appendChild(newLand);
+		newSection.appendChild(tempArticle);
+	}
+	//appends the entire section to the body of the document
+	document.body.appendChild(newSection);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
