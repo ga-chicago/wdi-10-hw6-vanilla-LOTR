4d075429b235d8d96dd709a1a1928efce537476c
sergio <scrummish@gmail.com>
2017-12-18 16:30:50 -0600

    Commit Part 1

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -19,5 +19,25 @@ var buddies = [
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 
+var makeMiddleEarth = function () {
+   // create a section tag with an id of `middle-earth`
+   let middleEarth = document.createElement("section");
+   middleEarth.setAttribute("id", "middle-earth")
+   console.log(middleEarth)
+   // add each land as an `article` tag
+   for(let i = 0; i < lands.length; i++){
+   		let landArticle = document.createElement("article");
+   		console.log(landArticle);
+   		landArticle.innerHTML = "<h1>" + lands[i] + "</h1>";
+   		document.body.appendChild(landArticle)
+   }
+   // inside each `article` tag include an `h1` with the name of the land
+
+
+   // append `middle-earth` to your document `body`
+   document.body.appendChild(middleEarth);
+};
+
+makeMiddleEarth();
 
 
