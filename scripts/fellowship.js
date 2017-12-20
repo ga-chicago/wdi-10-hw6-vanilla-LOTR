2e2c2ffde2c4e5956b37507eb2925ca322a43ead
ethan375 <ethanebel1@gmail.com>
2017-12-20 07:45:07 -0600

    commit 1

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -19,5 +19,46 @@ var buddies = [
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 
+var makeMiddleEarth = function () {
+  let newSection = document.createElement('section');
+  newSection.id = 'middle-earth';
+   
+   // add each land as an `article` tag
+   for(let i=0; i < lands.length; i++){
+    let newArticle = document.createElement('article')
+    newArticle.setAttribute('id', 'lands[i]')
+   // inside each `article` tag include an `h1` with the name of the land
+   let headTag = document.createElement('h1')
+   headTag.innerText = lands[i]
+   // append `middle-earth` to your document `body`
+   newSection.appendChild(newArticle)
+   newArticle.appendChild(headTag);
+ }
+ document.body.appendChild(newSection)
+};
+makeMiddleEarth();
+
+
+var makeHobbits = function () {
+   // display an `unordered list` of hobbits in the shire
+   let ul = document.createElement('ul')
+   for(let i=0; i<hobbits.length; i++){
+    let newHobbit = document.createElement('li');
+    newHobbit.innerText = hobbits[i];
+    newHobbit.classList.add('hobbit');
+    ul.appendChild(newHobbit);
+      // (which is the second article tag on the page)
+   }
+   // give each hobbit a class of `hobbit`
+   document.getElementsById('The Shire').appendChild(newList)
+};
+
+makeHobbits();
+
+var keepItSecretKeepItSafe = function () {
+   // create a div with an id of `'the-ring'`
+   // give the div a class of `'magic-imbued-jewelry'`
+   // add the ring as a child of `Frodo`
+};
 
 
